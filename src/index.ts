4bb4fae4bea5578ee42e22e07baed2efd9367de5
isv-anh <ndh-anh@vnisv.com>
2026-08-04 14:11:03 +0700

    feat: add checkpointer integration with Postgres and update graph invocation

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,19 @@
 import "dotenv/config";
 import { HumanMessage } from "@langchain/core/messages";
 import { app } from "./agent/graph.js";
+import { checkpointer } from "./checkpointer/checkpointer.js";
 
+
+await checkpointer.setup();
 
 const result = await app.invoke({
   messages: [
-    new HumanMessage("Lấy nhiệt độ của HCM hiện tại + thêm 2"),
+    new HumanMessage("tôi sống ở đâu"),
   ],
+}, {
+  configurable: {
+    thread_id: "thread-1",
+  }
 });
 
 console.log(result.messages);
